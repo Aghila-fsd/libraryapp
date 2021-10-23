d9cab9e1a12d0c9a30478e24998d6abe13c9a45e
unknown <aghikrishna@gmail.com>
2021-10-23 17:48:49 +0530

    second 2

--- src/model/Authordata.js
+++ src/model/Authordata.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://USERONE:<USERONE@ictakfiles.rp8eh.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 //mongoose.connect('mongodb://localhost:27017/libraryapp');
-//mongoose.connect('mongodb+srv://userone:userone@ictakfiles.fmpzz.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 
 const Schema=mongoose.Schema;
 const AuthorSchema=new Schema({
--- src/model/Bookdata.js
+++ src/model/Bookdata.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://USERONE:<USERONE@ictakfiles.rp8eh.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 
-mongoose.connect('mongodb://localhost:27017/libraryapp');
+//mongoose.connect('mongodb://localhost:27017/libraryapp');
 
 const Schema=mongoose.Schema;
 const BookSchema=new Schema({
